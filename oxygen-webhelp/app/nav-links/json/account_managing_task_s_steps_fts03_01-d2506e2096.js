f3d6a460b0d640d9cd83c2b4b30763f519a221a4
yevab <yevab@Yevik>
2025-11-18 20:15:32 +0100

    additions

---
+++ oxygen-webhelp/app/nav-links/json/account_managing_task_s_steps_fts03_01-d2506e2096.js
@@ -0,0 +1,1 @@
+oxy_webhelp_navlinks = {"account_managing_task_s_steps_fts03_01-d2506e2096":{"topics":[{"title":"New Payment task steps: FTS03.01.01","href":"topics\/bof_1.html","attributes":{"data-id":"bof_1"},"menu":{"hasChildren":false},"tocID":"bof_1-d2506e2104","next":"bof_1-d2506e2104"}]}};
