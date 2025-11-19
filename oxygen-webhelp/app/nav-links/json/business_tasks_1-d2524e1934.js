c5bf92b54cf7a9c13b392b868752c4e8db36e35c
yevab <yevab@Yevik>
2025-11-19 10:37:42 +0100

    latest output

---
+++ oxygen-webhelp/app/nav-links/json/business_tasks_1-d2524e1934.js
@@ -0,0 +1,1 @@
+oxy_webhelp_navlinks = {"business_tasks_1-d2524e1934":{"topics":[{"title":"Functional Analysis (Confluence)","href":"topics\/functional_analysis_confluence.html","attributes":{"data-id":"functional_analysis_confluence"},"menu":{"hasChildren":false},"tocID":"functional_analysis_confluence-d2524e1945","topics":[]}]}};
